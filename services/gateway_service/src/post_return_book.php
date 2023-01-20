0f4444f5614342788839280d6e23e7c688d01eff
KseniaTya <56265098+kseniatya@users.noreply.github.com>
2023-01-20 14:22:42 +0300

    Исправлен баг, в результате которого в время перезапуска запроса убирались звезды

--- services/gateway_service/src/post_return_book.php
+++ services/gateway_service/src/post_return_book.php
@@ -19,40 +19,47 @@ try{
 
     // получаем старые данные резервации
     $old_reservations = json_decode(curl("http://gateway_service:80/api/v1/reservations", ['X-User-Name: '.getallheaders()['X-User-Name']]));
-    $old_reservations = array_filter($old_reservations, fn($x) => $x->reservationUid == $old_reservations);
-    $old_reservation = reset($old_reservations);
-
-    $reservationData = curl("http://reservation_system:80/return_book?username=$username&reservationUid=$reservationUid&date=$date");
-
-    if($reservationData == "[]"){
-        http_response_code(404);
-    }
-    else{
-        $arr = json_decode($reservationData);
-        // увеличить счетчик доступных книг
-        //$numBooks = curl("http://library_system:80/return_book?username=$username&reservationUid=$reservationUid");
-        curl("http://library_system:80/count_book?book_uid=$arr->book_uid&library_uid=$arr->library_uid&count=1");
-
-        $book = json_decode(curl("http://library_system:80/get_book_by_uid?book_uid=$arr->book_uid"));
-        $stars = 0;
-        if($arr->status == 'EXPIRED'){
-            $stars -= 10;
-        }
-        if($book->condition != $condition){
-            $stars -= 10;
-            curl("http://library_system:80/change_condition_book?book_uid=$arr->book_uid&condition=$condition");
-        }
-        if($stars == 0){
-            $stars+= 1;
-        }
-        curl("http://rating_system:80/change_rating?username=$username&stars=$stars");
-
-        $circuit->success();
+    print_r($old_reservations);
+    $old_reservations = array_filter($old_reservations, fn($x) => $x->reservationUid == $reservationUid);
+    $old_reservation = $old_reservations[array_key_first($old_reservations)]??null;
+    if(!isset($old_reservation) || $old_reservation->status != "RENTED"){
         http_response_code(204);
-        echo ("
-            \"condition\": \"$condition\",
-            \"date\": \"$arr->till_date\"
-        ");
+    }else{
+
+        $reservationData = curl("http://reservation_system:80/return_book?username=$username&reservationUid=$reservationUid&date=$date");
+
+        if($reservationData == "[]"){
+            http_response_code(404);
+        }
+        else{
+            $arr = json_decode($reservationData);
+            // увеличить счетчик доступных книг
+            //$numBooks = curl("http://library_system:80/return_book?username=$username&reservationUid=$reservationUid");
+            curl("http://library_system:80/count_book?book_uid=$arr->book_uid&library_uid=$arr->library_uid&count=1");
+
+            $book = json_decode(curl("http://library_system:80/get_book_by_uid?book_uid=$arr->book_uid"));
+            $stars = 0;
+            if($arr->status == 'EXPIRED'){
+                $stars -= 10;
+            }
+            if($book->condition != $condition){
+                $stars -= 10;
+                curl("http://library_system:80/change_condition_book?book_uid=$arr->book_uid&condition=$condition");
+            }
+            if($stars == 0){
+                $stars+= 1;
+            }
+            check_health("http://pstgu.yss.su/1/MorozIvan/test/index.php?data=stars_1_$stars");
+            curl("http://rating_system:80/change_rating?username=$username&stars=$stars");
+            check_health("http://pstgu.yss.su/1/MorozIvan/test/index.php?data=stars_2_$stars");
+
+            $circuit->success();
+            http_response_code(204);
+            echo ("
+                \"condition\": \"$condition\",
+                \"date\": \"$arr->till_date\"
+            ");
+        }
     }
 } catch (RuntimeException $e){
     $circuit->failure();
